5bd20f3cef0e8562ad75e7dd228aa49137e5a1c6
Aleisson <caioaleisson@gmail.com>
2022-06-11 21:42:49 -0300

    functions selecao

---
+++ script.js
@@ -0,0 +1,26 @@
+function selecionarPrato(elemento){
+    const botaoClicado = document.querySelector(".selecionado");
+    if(botaoClicado !== null){
+        botaoClicado.classList.remove("selecionado");
+    } 
+
+    elemento.classList.add("selecionado");
+}
+
+function selecionarBebida(elemento){
+    const botaoClicado = document.querySelector(".bebidas .selecionado");
+    if(botaoClicado !== null){
+        botaoClicado.classList.remove("selecionado");
+    } 
+
+    elemento.classList.add("selecionado");
+}
+
+function selecionarSobremesas(elemento){
+    const botaoClicado = document.querySelector(".sobremesas .selecionado");
+    if(botaoClicado !== null){
+        botaoClicado.classList.remove("selecionado");
+    } 
+
+    elemento.classList.add("selecionado");
+}
