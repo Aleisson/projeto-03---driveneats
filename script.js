46391f36150a171f30f1902a9855ce0043df03b5
Aleisson <caioaleisson@gmail.com>
2022-06-12 09:44:24 -0300

    functions botao

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-function selecionarPrato(elemento){
+let prato;
+let bebida;
+let sobremesa;
+
+function selecionarPrato(elemento) {
+    prato = elemento.innerHTML;
     const botaoClicado = document.querySelector(".selecionado");
-    if(botaoClicado !== null){
+    if (botaoClicado !== null) {
         botaoClicado.classList.remove("selecionado");
-    } 
-
+    }
     elemento.classList.add("selecionado");
+    fecharPedido();
 }
 
-function selecionarBebida(elemento){
+function selecionarBebida(elemento) {
+    bebida = elemento;
     const botaoClicado = document.querySelector(".bebidas .selecionado");
-    if(botaoClicado !== null){
+    if (botaoClicado !== null) {
         botaoClicado.classList.remove("selecionado");
-    } 
-
+    }
+    //alert(elemento.querySelector("p").innerHTML)
     elemento.classList.add("selecionado");
+    fecharPedido();
 }
 
-function selecionarSobremesas(elemento){
+function selecionarSobremesas(elemento) {
+    sobremesa = elemento;
     const botaoClicado = document.querySelector(".sobremesas .selecionado");
-    if(botaoClicado !== null){
+    if (botaoClicado !== null) {
         botaoClicado.classList.remove("selecionado");
-    } 
+    }
 
     elemento.classList.add("selecionado");
+    fecharPedido();
 }
+
+function fecharPedido() {
+    if (prato && bebida && sobremesa) {
+        const botao = document.querySelector(".botao");
+        botao.classList.add("fechado");
+        botao.innerHTML = "<h2>Fechar pedido";
+        botao.setAttribute("onclick", "teste()");
+    }
+}
+
+function teste() {
+    alert("OI");
+}
+
+confirm.log(`${encodeURIComponent("teste")}
+${encodeURIComponent("teste")}`)
